2c3f60cc39a1d714c7c84c0d881cdb0ebe451f7e
dsasdfasdfasdf2 <9010859@student.zadkine.nl>
2023-03-26 14:19:53 +0200

    done ?

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/../Assets/Toufik Groenendijk.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://online.flippingbook.com/view/482276142/";
+  "https://raw.githubusercontent.com/soumyajit4419/portfolio/blob/main/src/Assets/Toufik Groenendijk.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
