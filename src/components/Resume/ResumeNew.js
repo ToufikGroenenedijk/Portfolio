a3dd0578fbb0c610bba3442cfee09df4ba5374f1
dsasdfasdfasdf2 <9010859@student.zadkine.nl>
2023-03-26 14:41:32 +0200

    done

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/blob/main/src/Assets/Toufik Groenendijk.pdf";
+  "https://portfolio-nine-nu-94.vercel.app/static/media/Toufik%20Groenendijk.fa5fc87ea0daf5065ac1.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
